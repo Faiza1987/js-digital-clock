882a61f86b5c22a7034b4e3b3680243686e8dc5b
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-05-28 09:43:52 -0700

    Added day, date and year and added shangahi

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-// let liveClock = setInterval(clock, 1000);
+
 
 $(document).ready(function(){
   const seattleClock = function clock(){
 
-    // const monthNames = ["January", "February", "March",
-    //   "April", "May", "June", "July",
-    //   "August", "September", "October",
-    //   "November", "December" ];
-
     let date = new Date();
 
-    let time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
     let hour = date.getHours().toString();
     let minute = date.getMinutes().toString();
     let second = date.getSeconds().toString();
+    let mid = 'PM';
 
     if(hour > 12){
       hour -= 12;
+    } else {
+      mid = 'AM';
     }
 
     if(hour < 10){
@@ -28,11 +25,18 @@ $(document).ready(function(){
     if(second < 10){
       second = '0' + second;
     }
-    $('#clock').text(`local time: ${time}`);
-    
+    $('#clock').html(`<p>${date.toDateString()}</p>
+    <p>${hour}:${minute}:${second} ${mid}</p>`);
   }
   
+  const shanghaiClock = function shanghaiClock(){
+    let shanghaiTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Shanghai"});
+    shanghaiTime = new Date(shanghaiTime.toLocaleString());
+
+    $('#shanghai-clock').text(`Shanghai time: ${shanghaiTime}`);
+  }
   setInterval(seattleClock, 1000);
+  setInterval(shanghaiClock, 1000);
   
 });
 
